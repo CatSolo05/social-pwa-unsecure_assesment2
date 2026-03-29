ccc4217543bc3bffa1e707cf54ba8355e400ff5d
CatSolo05 <132050108+CatSolo05@users.noreply.github.com>
2026-03-29 09:55:51 +0000

    changed app.run to bind to localhost instead of all interfaces

--- main.py
+++ main.py
@@ -174,4 +174,4 @@ def success():
 if __name__ == "__main__":
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
